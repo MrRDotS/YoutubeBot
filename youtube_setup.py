ca45ce90cbde622ddfd750ec3d136fb54838c9b1
Raahul S <raahul.sivanathan@gmail.com>
2023-07-16 14:59:09 -0400

    Split modules, made minor changes

---
+++ youtube_setup.py
@@ -0,0 +1,19 @@
+import yt_dlp
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '128',
+    }],
+    'limit-rate': '1m',
+    'default_search': 'ytsearch',
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
+}
+
+ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
